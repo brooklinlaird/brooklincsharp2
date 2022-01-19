9e3e68107113155d994ed5f15e4cae56c985caee
brooklinlaird <blaird2023@chadwickschool.org>
2022-01-18 21:40:51 -0800

    pt 1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
-
-namespace brooklincsharp2
-{
-    class Program
+﻿class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           char ch;
+           ch = 'A';
+           Console.WriteLine($"ch is the storing value: {ch}");
+
+           int ch_decimal;
+           ch_decimal = ch;
+           Console.WriteLine($"The decimal Value of ch is: {ch_decimal}");
+
+           int ToConvert;
+           Console.WriteLine("Enter An Integer: ");
+           ToConvert = int.Parse(Console.ReadLine());
+
+           char asChar;
+           asChar = (char)ToConvert;
+           Console.WriteLine($"The integer {ToConvert} cast to a char {asChar}");
+
+           char b;
+           b = (char) ('A' + 1);
+           Console.WriteLine($"'a' + 1 = {b}");
+            }
+
+
         }
     }
-}
